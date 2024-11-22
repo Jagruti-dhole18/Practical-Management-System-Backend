dc081433ec5b95a83937d997455b3c4258af2ec7
Jagruti-dhole18 <jagrutidhole18@gmail.com>
2024-11-22 19:04:48 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,18 +5,23 @@ import router from "../routes/FeedbackRoutes.js";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 
 //mounting api routes
 app.use("/api/v1",router)
 
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
+
 dotenv.config();
 
 dbConnect();
-
-
 
 app.listen(PORT, () => {
   console.log("Server is running at port:", PORT);
